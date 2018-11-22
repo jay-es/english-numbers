44018e5f83e63f47a1bd0e6c0e84bec9e9a1f9f7
Jun Shindo <jshindo@gladd.jp>
2018-11-22 23:46:49 +0900

    lint autofix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,20 @@ const env = process.env.NODE_ENV === 'production' ? 'error' : 'off'
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/recommended',
     '@vue/standard',
     '@vue/prettier',
-    '@vue/typescript'
+    '@vue/typescript',
   ],
   rules: {
     'no-console': env,
     'no-debugger': env,
-    '@vue/prettier': [env, require('./.prettierrc')]
+    '@vue/prettier': [env, require('./.prettierrc')],
   },
   parserOptions: {
-    parser: 'typescript-eslint-parser'
-  }
+    parser: 'typescript-eslint-parser',
+  },
 }
--- src/main.ts
+++ src/main.ts
@@ -11,5 +11,5 @@ Vue.use(Buefy)
 new Vue({
   router,
   // store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
